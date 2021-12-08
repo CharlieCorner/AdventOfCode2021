b61744800e77a11cac82e1e85963e7d555e6647b
Charlie Corner <CharlieCorner@users.noreply.github.com>
2021-12-08 14:00:07 -0600

    Add AdventDay abstract class to utils

--- advent_utils.py
+++ advent_utils.py
@@ -1,6 +1,26 @@
+from abc import ABC, abstractmethod
+
 def read_input_file(filename: str) -> list:
 
     with open(filename) as fp:
         lines = fp.readlines()
     
     return lines
+
+class AdventDay(ABC):
+
+    @staticmethod
+    def read_input(filename: str):
+        return read_input_file(filename)
+    
+    @abstractmethod
+    def parse_input(self, lines: list) -> list:
+        raise NotImplementedError
+    
+    @abstractmethod
+    def part_1(self):
+        raise NotImplementedError
+    
+    @abstractmethod
+    def part_2(self):
+        raise NotImplementedError
