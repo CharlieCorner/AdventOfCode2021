3690f150a6fc124394ace67c75b307f1b7f90e80
Charles Corner <charliecorner@users.noreply.github.com>
2021-12-10 17:15:33 -0600

    Add filename to constructor to automatically parse input

--- advent_utils.py
+++ advent_utils.py
@@ -1,26 +1,32 @@
 from abc import ABC, abstractmethod
 
+
 def read_input_file(filename: str) -> list:
 
     with open(filename) as fp:
         lines = fp.readlines()
-    
+
     return lines
 
+
 class AdventDay(ABC):
+
+    def __init__(self, filename: str = None) -> None:
+        if filename:
+            self.parsed_input = self.parse_input(self.read_input(filename))
 
     @staticmethod
     def read_input(filename: str):
         return read_input_file(filename)
-    
+
     @abstractmethod
     def parse_input(self, lines: list) -> list:
         raise NotImplementedError
-    
+
     @abstractmethod
     def part_1(self):
         raise NotImplementedError
-    
+
     @abstractmethod
     def part_2(self):
         raise NotImplementedError
