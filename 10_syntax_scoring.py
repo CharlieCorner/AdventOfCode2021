bc51ae861f3d3b3f991b02a4bdc4f760196912a5
Charlie Corner <CharlieCorner@users.noreply.github.com>
2021-12-10 00:44:57 -0600

    Solve day 10 of advent

--- 10_syntax_scoring.py
+++ 10_syntax_scoring.py
@@ -1,7 +1,7 @@
 from advent_utils import AdventDay
 
 
-class Day10(AdventDay):
+class Chunk:
 
     illegal_char_scores = {
         ")": 3,
@@ -10,28 +10,81 @@ class Day10(AdventDay):
         ">": 25137
     }
 
+    closing_char_scores = {
+        ")": 1,
+        "]": 2,
+        "}": 3,
+        ">": 4
+    }
+
+    def __init__(self,
+                 chunk: str,
+                 is_corrupt: bool,
+                 stack: list,
+                 first_illegal_char: str = None) -> None:
+        self.chunk = chunk
+        self.is_corrupt = is_corrupt
+        self.is_incomplete = not is_corrupt
+        self.first_illegal_char = first_illegal_char
+        self.stack = stack
+
+        if is_corrupt:
+            self.score = self._calculate_corrupt_score()
+        else:
+            self.missing_chars = self._calculate_missing_chars()
+            self.score = self._calculate_incomplete_score()
+
+    def _calculate_corrupt_score(self) -> int:
+        return Chunk.illegal_char_scores[self.first_illegal_char]
+
+    def _calculate_incomplete_score(self) -> int:
+        score = 0
+
+        for char in self.missing_chars:
+            score *= 5
+            score += Chunk.closing_char_scores[char]
+
+        return score
+
+    def _calculate_missing_chars(self) -> list:
+        missing = []
+        for open_char in self.stack[::-1]:
+            if open_char == "(":
+                missing.append(")")
+            elif open_char == "[":
+                missing.append("]")
+            elif open_char == "{":
+                missing.append("}")
+            elif open_char == "<":
+                missing.append(">")
+        return missing
+
+    def __str__(self) -> str:
+        return self.chunk
+
+
+class Day10(AdventDay):
+
     def parse_input(self, lines: list) -> list:
         return [l.strip() for l in lines]
 
     def part_1(self):
         chunks = self.parse_input(self.read_input("10_input.txt"))
-        final_score = self._calculate_score(self._first_illegal_chars(chunks))
+        final_score = sum(
+            [c.score for c in self._process_chunks(chunks) if c.is_corrupt])
         print(f"Final score: {final_score}")
 
     def part_2(self):
-        chunks = self.parse_input(self.read_input("10_test.txt"))
-        final_score = self._calculate_score(self._first_illegal_chars(chunks))
-        print(f"Final score: {final_score}")
+        chunks = self.parse_input(self.read_input("10_input.txt"))
 
-    def _calculate_score(self, illegal_chars: list) -> list:
-        result = 0
+        scores = [c.score for c in self._process_chunks(
+            chunks) if c.is_incomplete]
+        
+        scores.sort()
 
-        for ic in illegal_chars:
-            result += Day10.illegal_char_scores[ic]
+        print(f"Final score: {scores[len(scores)//2]}")
 
-        return result
-
-    def _first_illegal_chars(self, chunks: list) -> list:
+    def _process_chunks(self, chunks: list) -> list:
         legal_pairs = {
             "()": None,
             "{}": None,
@@ -43,13 +96,14 @@ class Day10(AdventDay):
 
         for chunk in chunks:
             brace_stack = []
+            chunk_is_corrupted = False
 
             for char in chunk:
 
                 if not brace_stack:
                     brace_stack.append(char)
                     continue
-                
+
                 # Open braces are added to the stack, no questions asked
                 if char in ["(", "{", "[", "<"]:
                     brace_stack.append(char)
@@ -59,17 +113,22 @@ class Day10(AdventDay):
                 #  the entry is corrupted
                 if char in [")", "}", "]", ">"]:
                     if brace_stack[-1] + char in legal_pairs:
-                        brace_stack.pop() # We have closed a pair
+                        brace_stack.pop()  # We have closed a pair
                     else:
-                        result.append(char)
+                        result.append(Chunk(chunk,
+                                            is_corrupt=True,
+                                            stack=brace_stack,
+                                            first_illegal_char=char))
+                        chunk_is_corrupted = True
                         break
+            # If there's still something in the stack and the chunk is not corrupted,
+            #  it is an incomplete chunk
+            if not chunk_is_corrupted and brace_stack:
+                result.append(Chunk(chunk,
+                                    is_corrupt=False,
+                                    stack=brace_stack))
         return result
 
 
-
-
-
-
-
 if __name__ == "__main__":
-    Day10().part_1()
+    Day10().part_2()
