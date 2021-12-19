50e7a412e2d424f4e9a82a32519b0e70d0e6a7a9
Charlie Corner <CharlieCorner@users.noreply.github.com>
2021-12-19 01:40:46 -0600

    Add methods to parse each type of Packets

--- 16_packet_decoder.py
+++ 16_packet_decoder.py
@@ -1,6 +1,8 @@
 from __future__ import annotations
+from types import DynamicClassAttribute
 
 from advent_utils import AdventDay
+
 
 def hexadecimal_2_binary(hexadecimal: str) -> str:
     hexa_mapping = {
@@ -25,40 +27,84 @@ def hexadecimal_2_binary(hexadecimal: str) -> str:
 
     for h in hexadecimal:
         binary += hexa_mapping[h]
-    
+
     return binary
+
 
 def binary_2_decimal(binary: str) -> int:
     return int(binary, 2)
 
+
 class PacketParser:
-    
+
     @staticmethod
     def parse_packet(binary: str) -> Packet:
+        # Parse the header with the version and type
+        version = binary_2_decimal(binary[:3])
+        type = binary_2_decimal(binary[3:6])
+        data = binary[6:]
+
+        if type == 4:
+            return LiteralValuePacket(version=version,
+                                      data=data)
+        else:
+            return OperatorPacket(version=version,
+                                  type_id=type,
+                                  data=data)
+
+    @staticmethod
+    def detect_packet_boundaries(binary: str) -> list:
         raise NotImplementedError
+
 
 class Packet:
     def __init__(self, version: int, type_id: int) -> None:
         self.version = version
         self.type_id = type_id
         self.subpackets = []
+        self.value = None
 
 
 class LiteralValuePacket(Packet):
-    def __init__(self, version: int, value_bits: str) -> None:
-        self.value_bits = binary_2_decimal(value_bits)
+    def __init__(self, version: int, data: str) -> None:
         super().__init__(version, type_id=4)  # All LiteralValue packets are ID = 4
+
+        self.value = self._parse_value(data)
+
+    def _parse_data(self, data):
+        continue_parsing_data = True
+        index = 0
+        val = ""
+
+        while continue_parsing_data:
+            val += data[index + 1:index + 5]
+            continue_parsing_data = data[index] == "1"
+            index += 5
+        return binary_2_decimal(val)
 
 
 class OperatorPacket(Packet):
-    def __init__(self, version: int, type_id: int, length_type_id:int , subpacket_data: str) -> None:
-        
-        self.length_type_id = length_type_id
-        self.is_number_of_packets = length_type_id == 1
-
-        self.length = 11 if self.is_number_of_packets else 15
-
+    def __init__(self, version: int, type_id: int, data: str) -> None:
         super().__init__(version, type_id)
+
+        self.length_type_id = int(data[0])
+        self.is_number_of_packets = self.length_type_id == 1
+
+        # Remove the bit that we have consumed
+        data = data[1:]
+
+        if self.is_number_of_packets:
+            num_packets = binary_2_decimal(data[:11])
+            data = data[11:]
+
+            for _ in range(num_packets):
+                self.subpackets.append(PacketParser.parse_packet(data[:11]))
+                data = data[11:]
+        else:
+            bits_in_subpackets = binary_2_decimal(data[:15])
+            data = data[15:]
+            self.subpackets.extend(
+                PacketParser.detect_packet_boundaries(data[:bits_in_subpackets]))
 
 
 class Day16(AdventDay):
@@ -70,13 +116,11 @@ class Day16(AdventDay):
 
         for transmision in self.parsed_input:
             print(f"Transmission: {transmision}")
-            
+
             binary_rep = hexadecimal_2_binary(transmision)
             print(f"Binary: {binary_rep}")
 
             packet = PacketParser.parse_packet(binary_rep)
-        
-        
 
     def part_2(self):
         return super().part_2()
